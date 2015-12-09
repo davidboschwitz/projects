e63811c1a90346a5401119e251f51cc17743d6e4
David Boschwitz <hihid27@aim.com>
2015-12-09 01:14:16 -0600

    initial commit

---
+++ node.js/helloworld.js
@@ -0,0 +1,15 @@
+var HelloWorld = function(){};
+
+HelloWorld.prototype.sayhello = function (response){
+  // Send the HTTP header
+  // HTTP Status: 200 : OK
+  // Content Type: text/plain
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+
+  // Send the response body as "Hello World"
+
+  //response.write(request.rawHeaders);
+  response.end('Hello World\n');
+}
+
+module.exports = new HelloWorld();
---
+++ node.js/server.js
@@ -0,0 +1,33 @@
+var http = require("http");
+var session = require('client-sessions');
+var helloworld = require("./helloworld.js");
+
+app.use(session({
+  cookieName: 'session',
+  secret: 'random_string_goes_here',
+  duration: 30 * 60 * 1000,
+  activeDuration: 5 * 60 * 1000,
+}));
+
+http.createServer(function (request, response) {
+  var get_data = require('url').parse(request.url);
+
+  console.log(get_data);
+  console.log(get_data['pathname']);
+
+  if(get_data['pathname'] == "/hello"){
+    helloworld.sayhello(response);
+  }
+  switch(get_data['pathname']){
+    case "/hello":
+      helloworld.sayhello(response);
+      break;
+    case "/":
+      response.writeHead(200, {'Content-Type': 'text/plain'});
+      response.end("Nothing here yet");
+      break;
+  }
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://localhost:8081/');
